bce95e0a1d9238fa8a931ebbf78895aa981a00f2
Amey <ameyks.dev1809@gmail.com>
2021-10-27 19:06:52 -0400

    Forecast parameter added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude,longitude, callback) =>{
+    console.log(latitude +','+longitude)
     const url = 'http://api.weatherstack.com/current?access_key=134de323400efa75900a0bdf0adef041&query='+ latitude +','+ longitude +'&units=f'
 
 // //const error_url = 'http://api.weatherstack.com/current?access_key=134de323400efa75900a0bdf0adef041&query=&units=f'
@@ -14,10 +15,11 @@ const forecast = (latitude,longitude, callback) =>{
             const weatherForecast = {
                 weather: body.current.weather_descriptions[0],
                 temp : body.current.temperature,
-                apprentTemp: body.current.feelslike
+                apprentTemp: body.current.feelslike,
+                visibility : body.current.visibility
             }
             const result =  weatherForecast.weather +'. It is ' +  weatherForecast.temp
-            + ' degrees out. But feels like '+  weatherForecast.apprentTemp +' degrees.'
+            + ' degrees out. But feels like '+  weatherForecast.apprentTemp +' degrees. Visibility is '+ weatherForecast.visibility+'.!'
             callback(undefined, result)
         }    
     })
